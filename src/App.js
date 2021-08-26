9f3dac2dc220ea0a91b9cb8d8f6f12c490f8717b
abhaya kumar sahoo <61579704+abhaya-kumar-sahoo@users.noreply.github.com>
2021-08-27 00:43:23 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import { Form, Row, FormGroup, Col } from "react-bootstrap";
+import RangeSlider from 'react-rangeslider'
+import { render } from 'react-dom';
+import './App.css'
 
 function App() {
+  const [redColor, setRedColor] = useState(0)
+  const [greenColor, setGreenColor] = useState(0)
+  const [blueColor, setBlueColor] = useState(0)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="controller" style={{
+      backgroundColor:"rgb("+redColor+","+greenColor+","+blueColor+")",
+      height:"100vh",   
+      }}>
+      <h1>Colour Picker </h1>
+      <h2>Abhaya Kumar Sahoo</h2>
+      <div className="box">
+
+      <h4>Red:</h4><br/>    
+      <input class="slider" type="range" max="255" onChange={e => setRedColor(e.target.value)} value={redColor} /><br/>
+  
+      {redColor}
+
+     <h4>Green: </h4><br/> 
+      <input class="slider" type="range" max="255" onChange={e => setGreenColor(e.target.value)} value={greenColor}/><br/>
+      {greenColor} 
+      <h4>Blue:</h4><br/> 
+          
+      <input class="slider" type="range" max="255" onChange={e => setBlueColor(e.target.value)} value={blueColor}/><br/>
+      {blueColor}
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
